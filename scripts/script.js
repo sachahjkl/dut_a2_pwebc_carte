d8722142f0a7c04c67a77082d67bee960d5128ee
sachafroment <sacha.froment@gmail.com>
2019-03-12 09:22:50 +0100

    zoom smooth

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,8 @@ function initMap(xInit, yInit, zoom) {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         minZoom: 12,
+        zoomSnap : 0,
+        zoomDelta : 1,
         id: 'mapbox.streets',
         accessToken: token
     }).addTo(mymap);
